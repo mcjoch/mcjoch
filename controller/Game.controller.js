35f6a4b8f61807cad98fe07ccecdc0f7b0dc6502
mjochemsen <marinus.jochemsen@un.org>
2020-06-12 21:09:30 -0400

    Added dialog to select number of players

--- controller/Game.controller.js
+++ controller/Game.controller.js
@@ -9,29 +9,54 @@ sap.ui.define([
 	return Controller.extend("mj.toepen.controller.App", {
 
 		formatter: formatter,
+		game: {},
 
-		onInit: function () {
-			this.game = new Game(this, 4);
+		onInit: function() {
+			this.game = new Game(this);
 			this.setModel(new JSONModel(this.game), "gameModel");
+			
+			this.getRouter().getTarget("home").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
 		},
 		
+		handleRouteMatched: function() {
+			this.openStartGameDialog();
+		},
+
+		_getStartGameDialog: function() {
+			if (!this._oDialog) {
+				this._oDialog = sap.ui.xmlfragment("mj.toepen.view.StartGame", this);
+				this.getView().addDependent(this._oDialog);
+			}
+			return this._oDialog;
+		},
+		
+		openStartGameDialog: function() {
+			this._getStartGameDialog().open();
+		},
+	
+		startNewGame: function(oEvent, iNumPlayers) {
+			this.game.start(iNumPlayers);
+			this._getStartGameDialog().close();
+			this.redraw();
+		},
+
 		selectCard: function(oEvent) {
-			
+
 			// Don't play card outside turn
 			if (!this.game.player.currentTurn) {
 				return;
 			}
-			
+
 			var oSelectedCard = oEvent.getSource().getBindingContext("gameModel").getObject();
-			
+
 			this.game.player.playCard(oSelectedCard);
 		},
-		
+
 		redraw: function() {
 			document.activeElement.blur();
 			this.getModel("gameModel").updateBindings();
 		},
-	
+
 		/**
 		 * Convenience method for getting the view model by name in every controller of the application.
 		 * @public
@@ -41,7 +66,7 @@ sap.ui.define([
 		getModel: function(sName) {
 			return this.getView().getModel(sName);
 		},
-		
+
 		/**
 		 * Convenience method for setting the view model in every controller of the application.
 		 * @public
@@ -51,6 +76,13 @@ sap.ui.define([
 		 */
 		setModel: function(oModel, sName) {
 			return this.getView().setModel(oModel, sName);
+		},
+		
+		/**
+		 * Returns the application router
+		 */
+		getRouter: function() {
+			return sap.ui.core.UIComponent.getRouterFor(this);
 		}
 
 	});
